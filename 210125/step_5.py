68899865cbdd8df9a6b832554c78594367b1edec
Jo <jo@local>
2021-01-25 11:56:52 +0500

    RE_EMAIL_VALIDATOR

---
+++ 210125/step_5.py
@@ -0,0 +1,12 @@
+import re
+
+RE_EMAIL_VALIDATOR = re.compile(r'^[A-Za-z0-9._-]+@([a-z0-9._-]+\.)+[a-z]+$')  # \n\r, .com .org .su
+
+
+def email_is_valid(email):
+    return RE_EMAIL_VALIDATOR.match(email)
+
+
+assert email_is_valid('ivan@kss45.kpk.ru')
+assert not email_is_valid('ivan@kss45')
+assert not email_is_valid('ivan@kss45.')
