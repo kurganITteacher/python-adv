107271e6b2067401a1d1cc6d996f86a5dbb81ba9
Jo <jo@local>
2021-01-25 12:12:43 +0500

    RE_DATA_VALIDATOR (refactored)

--- 210125/step_6.py
+++ 210125/step_6.py
@@ -1,7 +1,9 @@
 import re
 
 # dd.mm.yyyy
-RE_DATA_VALIDATOR = re.compile(r'^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$')
+# RE_DATA_VALIDATOR = re.compile(r'^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$')
+# RE_DATA_VALIDATOR = re.compile(r'^([0-9]{2}\.){2}[0-9]{4}$')
+RE_DATA_VALIDATOR = re.compile(r'^(\d{2}\.){2}\d{4}$')
 
 
 def data_is_valid(data):
