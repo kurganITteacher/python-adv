7e210c51906f9d7c11c33af4cbeda097ba082505
Jo <jo@local>
2021-01-25 12:17:18 +0500

    RE_DATA_VALIDATOR (upd)

--- 210125/step_6.py
+++ 210125/step_6.py
@@ -1,9 +1,10 @@
 import re
 
-# dd.mm.yyyy
+# dd.mm.yyyy | dd-mm-yyyy | dd/mm/yyyy
 # RE_DATA_VALIDATOR = re.compile(r'^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$')
 # RE_DATA_VALIDATOR = re.compile(r'^([0-9]{2}\.){2}[0-9]{4}$')
-RE_DATA_VALIDATOR = re.compile(r'^(\d{2}\.){2}\d{4}$')
+# RE_DATA_VALIDATOR = re.compile(r'^(\d{2}\.){2}\d{4}$')
+RE_DATA_VALIDATOR = re.compile(r'^(\d{2}[./-]){2}\d{4}$')
 
 
 def data_is_valid(data):
@@ -11,6 +12,8 @@ def data_is_valid(data):
 
 
 assert data_is_valid('25.01.2021')
+assert data_is_valid('25-01-2021')
+assert data_is_valid('25/01/2021')
 assert not data_is_valid('5.01.2021')
 assert not data_is_valid('05.01.21')
 assert not data_is_valid('05,01,2021')
