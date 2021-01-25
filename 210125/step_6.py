feab769b2afb777017c9627a31d5208c060ab6ba
Jo <jo@local>
2021-01-25 12:09:02 +0500

    RE_DATA_VALIDATOR

---
+++ 210125/step_6.py
@@ -0,0 +1,14 @@
+import re
+
+# dd.mm.yyyy
+RE_DATA_VALIDATOR = re.compile(r'^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$')
+
+
+def data_is_valid(data):
+    return RE_DATA_VALIDATOR.match(data)
+
+
+assert data_is_valid('25.01.2021')
+assert not data_is_valid('5.01.2021')
+assert not data_is_valid('05.01.21')
+assert not data_is_valid('05,01,2021')
