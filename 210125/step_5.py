c5809332e0a977b64d04a7c611ddca88323a3db5
Jo <jo@local>
2021-01-25 12:00:41 +0500

    RE_EMAIL_VALIDATOR (bug fixed)

--- 210125/step_5.py
+++ 210125/step_5.py
@@ -1,6 +1,6 @@
 import re
 
-RE_EMAIL_VALIDATOR = re.compile(r'^[A-Za-z0-9._-]+@([a-z0-9._-]+\.)+[a-z]+$')  # \n\r, .com .org .su
+RE_EMAIL_VALIDATOR = re.compile(r'^[A-Za-z0-9._-]+@([a-z0-9._-]+\.)+[a-z]{2,}$')  # \n\r, .com .org .su
 
 
 def email_is_valid(email):
@@ -8,5 +8,6 @@ def email_is_valid(email):
 
 
 assert email_is_valid('ivan@kss45.kpk.ru')
+assert not email_is_valid('ivan@kss45.kpk.r')
 assert not email_is_valid('ivan@kss45')
 assert not email_is_valid('ivan@kss45.')
