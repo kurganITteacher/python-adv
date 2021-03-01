f026d6075a8bce0baeeb69feae2020ee098adc9f
Ревняков Евгений <revnyakov@ergosolo.ru>
2021-03-01 12:02:57 +0500

    docker (upd 2)

--- 210301/build/docker/sbin/run.sh
+++ 210301/build/docker/sbin/run.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
 echo "Starting kpk docker"
-python3 "KPK_BIN/django/..." &
-P1=$!
-
-wait $P1
+python3 "$KPK_BIN/timetable/app.py"
--- 210301/build/start.sh
+++ 210301/build/start.sh
@@ -7,5 +7,5 @@ echo "Starting kpk docker"
 docker run --rm -it \
     -v $(pwd)/sources:/home/kpk/bin \
     -v $(pwd)/data:/home/kpk/data \
-    kpk sh
+    kpk
 
