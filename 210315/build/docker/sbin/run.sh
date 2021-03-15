2a3b218b1cfebf7c281be3c92c6dce512cde2f65
Ревняков Евгений <revnyakov@ergosolo.ru>
2021-03-15 11:43:46 +0500

    docker -d

--- 210315/build/docker/sbin/run.sh
+++ 210315/build/docker/sbin/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Starting kpk docker"
-#python3 "$KPK_BIN/timetable/app.py"
+python3 "$KPK_BIN/kpk_messenger/manage.py" runserver 0.0.0.0:8000
--- 210315/build/start.sh
+++ 210315/build/start.sh
@@ -3,9 +3,10 @@
 echo "Building kpk docker"
 docker build -f ./build/docker/Dockerfile -t kpk ./build/docker/
 
-#echo "Starting kpk docker"
-#docker run --rm -it \
-#    -v $(pwd)/sources:/home/kpk/bin \
-#    -v $(pwd)/data:/home/kpk/data \
-#    kpk
+echo "Starting kpk docker"
+docker run --rm -d \
+    -v $(pwd)/sources:/home/kpk/bin \
+    -v $(pwd)/data:/home/kpk/data \
+    -p 8011:8000 \
+    kpk
 
