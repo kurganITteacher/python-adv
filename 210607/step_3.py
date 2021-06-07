79a6449f942357cc819717025f93c895559f34a5
Jo <jo@local>
2021-06-07 11:26:50 +0500

    Student

--- 210607/step_3.py
+++ 210607/step_3.py
@@ -3,4 +3,10 @@
 # * дата рождения
 # * адрес
 
+class Student:
+
+    def __init__(self, name, birth_date, address):
+        self.name = name
+        self.birth_date = birth_date
+        self.address = address
 
