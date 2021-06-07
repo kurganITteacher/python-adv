5eb3e669195334d9ee7d4118e3c4b1583d1683a4
Jo <jo@local>
2021-06-07 11:45:34 +0500

    TeachGroup

--- 210607/step_3.py
+++ 210607/step_3.py
@@ -2,11 +2,22 @@
 # * имя
 # * дата рождения
 # * адрес
+#
+# класс учебной группы (композиция)
+# * название
+# * студенты
+# * методы для добавления, удаления и просмотра студентов группы
+
 
 class Student:
-
     def __init__(self, name, birth_date, address):
         self.name = name
         self.birth_date = birth_date
         self.address = address
 
+
+class TeachGroup:
+    def __init__(self, name):
+        self.name = name
+        self.students = []
+        # self.students = {}
