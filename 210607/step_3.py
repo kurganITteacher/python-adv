1b83f25b045ce2945e4327f102bad76d591fd8a3
Jo <jo@local>
2021-06-07 12:18:22 +0500

    TeachGroup (methods)

--- 210607/step_3.py
+++ 210607/step_3.py
@@ -21,3 +21,31 @@ class TeachGroup:
         self.name = name
         self.students = []
         # self.students = {}
+
+    def add(self, student):
+        self.students.append(student)
+
+    def remove(self, student):
+        if student in self.students:
+            self.students.remove(student)
+
+    def show(self):
+        print(self.name, self.students)
+
+
+student_1 = Student('Иванов Иван Иванович', '15.05.2002', 'г.Курган, центр')
+student_2 = Student('Сергеев Сергей Сергеевич', '04.11.2003', 'г.Курган, центр')
+group_1 = TeachGroup('ИТ-33')
+group_1.add(student_1)
+group_1.add(student_2)
+# print(group_1.show())
+# group_1.show()
+# group_1.remove(student_2)
+# group_1.show()
+
+for el in group_1.students:
+    print(el.birth_date)
+
+del group_1
+
+print(student_1)
