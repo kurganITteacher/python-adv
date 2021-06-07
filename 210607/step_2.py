9fed2b1731d51a7e1d437507de71a95978d64220
Jo <jo@local>
2021-06-07 11:15:59 +0500

    lesson 2

--- 210607/step_2.py
+++ 210607/step_2.py
@@ -1,2 +1,14 @@
 books = ['Я робот', 'Лунная пыль', 'Таинственный остров']
 
+# print(choice(books))
+
+# idx = 0
+# while idx < len(books):
+#     print(books[idx])
+#     idx += 1
+
+# for i in books:  # i, j, k - ? -> counters
+#     print(i)
+
+for el in books:
+    print(el)
