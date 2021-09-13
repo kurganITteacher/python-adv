f97c6b9021c0b344cf7687d488f5e0087872591f
Jo <jo@local>
2021-09-13 10:45:15 +0500

    App main component

--- dashboard/front/src/App.js
+++ dashboard/front/src/App.js
@@ -1,29 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);  // parent constructor
+        this.setState({
+            'users': [],
+            'projects': [],
+            'tasks': []
+        })
+    }
+
+    render() {
+        console.log('state', this.state);
+        return (
+            <div>
+                Dashboard
+            </div>
+        )
+    }
+
 }
 
 export default App;
-
-// function programming VS OOP
-// OOP -> object -> object STATE
-// function -> NO STATE
+// jsx -> js + ...
